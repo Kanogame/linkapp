87e8cf078a4d28145fceb4794df3cd209f8f3fe9
kanogame <wasdqert1222@gmail.com>
2023-03-06 20:39:02 +0700

    added node json parcing, card adding done

--- linkapp - backend/index.js
+++ linkapp - backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParcser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bodyParcser.json());
 
 const links = [
     {
@@ -28,9 +30,48 @@ const links = [
     }
 ];
 
+let nextId = Math.max(0, ...links.map(x => x.id)) + 1;
+
 app.get("/links/get", (req, res) => {
     res.json(links);
 });
+
+app.post("/link/add", (req, res) => {
+    const data = req.body;
+    const newLink = {
+        id: nextId,
+        title: data.name,
+        description: data.desc,
+        link: data.link,
+        favicon: generateFavicon(data.link),
+    };
+    links.push(newLink);
+    nextId++;
+    res.json({
+        success: true,
+    });
+});
+
+app.post("/link/remove", (req, res) => {
+    const data = +req.body;
+    links.splice(data, 1);
+})
+
+function generateFavicon(link) {
+    slashCnt = 0;
+    let res = link;
+    for (let i = 0; i < link.length; i++) {
+        let s = link[i];
+        if (s === "/") {
+            slashCnt++;
+            if (slashCnt === 3) {
+                res = link.slice(0, i);
+                break;
+            }
+        }
+    }
+    return res + "/favicon.ico";
+}
 
 app.listen(13532, () => {
     console.log("Сервер запущен на порту 13532");
